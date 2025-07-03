e6ef1194e5d76fb990bad8c4bdaffd3aaaba38ca
isma amjad <rashidjaveed1981@gmail.com>
2025-07-04 02:07:22 +0500

    assignment#2

---
+++ assignment#2.py
@@ -0,0 +1,49 @@
+#Declare a variable named age and assign it an integer value 25.
+#Print the value of age and its data type using the type() function.
+age=25
+print(f"Age is {age} and {type(age)}.")
+# Create a variable called temperature and assign it a float value 98.6.
+#Print the value of temperature and its data type.
+temprature=98.6
+print(f"temprture is {temprature} and {type(temprature)}.")
+# Declare a variable complex_num and assign it a complex number 3 + 4j.
+#Print the value of complex_num and its data type.
+complex_num=3+4j
+print(f"The complex number is {complex_num} and {type(complex_num)}.")
+# Create a variable named is_student and assign it the boolean value True.
+#Print the value of is_student and its data type.
+is_student=True
+print(f"The value of the variable is_student is {is_student} and is {type(is_student)}.")
+# Declare a variable called has_job and assign it the boolean value False.
+#Print the value of has_job and its data type.
+has_job=False
+print(f"The value of the variable has_job is {has_job} and {type(has_job)}.")
+# Create a variable greeting and assign it the string 'Hello' using single quotes.
+#Print the value of greeting and its data type.
+greeting='Hello'
+print(f"The value of the variable greetingt is {greeting} and {type(greeting)}.")
+# Declare a variable named city and assign it the string "New York" using double quotes.
+#Print the value of city and its data type.
+city="New york"
+print(f"The value of the variable city is {city} and {type(city)}.")
+# Create a variable quote and assign it the string 'Life is short' using triple single quotes.
+#Print the value of quote and its data type.
+quote='''Life is short'''
+print(f"Quote is \n{quote}\n and {type(quote)}.")
+# Declare a variable called poem and assign it a multi-line string using triple double quotes with the text:
+#Roses are red,
+#violets are blue.
+#Print the value of poem and its data type.
+poem=""""Roses are red,
+Violets are blue.
+"""
+print(f"Poem is \n{poem}\n and {type(poem)}")
+# **Create 3 variables:
+#score as an integer with value 95
+#height as a float with value 5.9
+#name as a string with value "Alice"
+#Print the value and data type of each variable.**
+score=95
+height=5.9
+name="Alice"
+print(f"Nmae={name} type={name.__class__}\nScore={score} type={score.__class__}\nHeight={height} type={height.__class__}")
