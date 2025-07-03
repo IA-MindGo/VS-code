e6bbca3b76477451dc62dc17e68a419ea893084e
isma amjad <rashidjaveed1981@gmail.com>
2025-07-04 02:04:20 +0500

    assignment#1

---
+++ Assignment#1.py
@@ -0,0 +1,35 @@
+# Print your name using the print() function.
+print("My name is 'isma amjad'")
+#Create a variable for your age and print it.
+age=36
+print(f"My age is {age}.")
+#Store your school or institute name in a variable and print it.
+institute="AI HUB"
+print(f"The name of my institute is {institute}.")
+#Add two numbers using variables and print the result.
+a=12
+b=23
+result=a+b
+print(f"The addition of two variables {a} and {b} is {result}.")
+#Store your name in a variable and print a welcome message.
+name="isma amjad"
+print(f"Welcome {name}")
+#Declare a variable for your city and print where you live.
+city="gujranwala"
+print(f"i live in {city}.")
+# Use multiple variables (name, age, city) and print a full sentence.
+name="isma amjad"
+age=36
+city="Gujranwala"
+print(f"My name is {name}, am {age} year old and live in {city}.")
+#Perform subtraction between two numbers using variables and print the answer.
+x=10
+y=5
+sub=x-y
+print(f"The subtraction of two variables {x} and {y} is {sub}.")
+#Store a motivational quote in a variable and print it.
+quote="If you can change your mind, you can change your life"
+print(quote)
+#Create a variable for your favorite subject and print it.
+subject="Artifical intelligence"
+print(f"My favourite subject is {subject}.")
