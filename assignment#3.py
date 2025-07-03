d858878da5e3d1c9c855c549acbc10ed0949686e
isma amjad <rashidjaveed1981@gmail.com>
2025-07-04 02:08:46 +0500

    assignment#3

---
+++ assignment#3.py
@@ -0,0 +1,64 @@
+#Declare a variable student_id with value 101 (integer) and a variable grades with a list: [85, 90, 92].
+#Print the value and type of both variables using the type() function.
+student_id=101
+grade=[85,90,92]
+print(f"The value of the variable 'student_id' is {student_id} and {type(student_id)}")
+print(f"The value of the variable 'grade' is {grade} and {type(grade)}")
+# Create a variable price with value 19.99 (float) and a variable dimensions with a tuple: (5.5, 3.2).
+#Print the value and type of both variables.
+price=19.99
+dimension=(5.5,3.2)
+print(f"The value of the variable 'price' is {price} and {type(price)}")
+print(f"The value of the variable 'dimension' is {dimension} and {type(dimension)}")
+#Declare a variable complex_value with a complex number 2 + 3j, and a variable numbers with range(1, 5).
+#Print the value and type of both variables.
+complex_value=2+3j
+number=range(1,5)
+print(f"The value of the variable 'complex_value' is {complex_value} and {type(complex_value)}")
+print(f"The value of the variable 'number' is {number} and {type(number)}")
+#Create a variable is_active with boolean value True, and a variable unique_ids with a set: {1, 2, 3}.
+#Print the value and type of both variables.
+is_active=True
+unique_ids={1,2,3}
+print(f"The value of the variable 'is_active' is {is_active} and {type(is_active)}")
+print(f"The value of the variable 'unique_ids' is {unique_ids} and {type(unique_ids)}")
+#Declare a variable status with a string 'Active' using single quotes, and a variable fixed_colors with a frozenset containing "red" and "blue".
+#Print the value and type of both variables.
+status='Active'
+fixed_colors=frozenset(["red","blue"])
+print(f"The value of the variable 'status' is {status} and {type(status)}")
+print(f"The value of the variable 'fixed_colors' is {fixed_colors} and {type(fixed_colors)}")
+#Create a variable country with a string "Canada" using double quotes, and a variable cities with a list: ["Toronto", "Vancouver"].
+#Print the value and type of both variables.
+country="Canada"
+cities=["Toronto","Vancouver"]
+print(f"The value of the variable 'country' is {country} and {type(country)}")
+print(f"The value of the variable 'cities' is {cities} and {type(cities)}")
+#Declare a variable motto with the string 'Keep it simple' using triple single quotes, and a variable coordinates with a tuple: (10, 20, 30).
+#Print the value and type of both variables.
+motto='''Keep it simple'''
+coordinates=(10,20,30)
+print(f"The value of the variable 'motto' is {motto} and {type(motto)}")
+print(f"The value of the variable 'coordinates' is {coordinates} and {type(coordinates)}")
+#Create a variable quantity with value 50 (integer), and a variable tags with a set: {"urgent", "new"}.
+#Print the value and type of both variables.
+quantity=50
+tags={"urgent","new"}
+print(f"The value of the variable 'quantity' is {quantity} and {type(quantity)}")
+print(f"The value of the variable 'tags' is {tags} and {type(tags)}")
+#Declare a variable distance with a float value 42.5, and a variable steps with range(0, 10, 2).
+#Print the value and type of both variables.
+distance=42.5
+steps=range(0,10,2)
+print(f"The value of the variable 'distance' is {distance} and {type(distance)}")
+print(f"The value of the variable 'steps' is {steps} and {type(steps)}")
+#**Create a variable note with a multi-line string using triple double quotes:
+#Meeting at 9 AM  
+#Bring notebook
+#and a variable locked_values with a frozenset: {100, 200, 300}.
+#Print the value and type of both variables.**
+note="""Meeting at 9 AM
+Bring notebook"""
+locked_values=frozenset([100,200,300])
+print(f"The value of the variable 'note' is {note} and {type(note)}")
+print(f"The value of the variable 'locked_values' is {locked_values} and {type(locked_values)}")
